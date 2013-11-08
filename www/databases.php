b36a213fc8b55b8447bd042c474dcb2e54b102b6
Kelsey Leftwich <kelseyleftwich@gmail.com>
2013-11-08 14:59:03 -0600

    Adds database config file

---
+++ www/databases.php
@@ -0,0 +1,25 @@
+<?php
+    /*code from: PHP with MySQL Essential Training with Kevin Skoglund
+    (http://www.lynda.com/course20/MySQL-tutorials/Connecting-MySQL-PHP/119003/137009-4.html)*/
+    
+    //1. Create a database connection
+    $dbhost = "localhost";
+    $dbuser = "kelseyle_kelsey";
+    $dbpass = "n0V3mber#13";
+    $dbname = "kelseyle_CS470-Coffee";
+    $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+?>
+    
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <title>untitled</title>
+    </head>
+
+
+    <body>
+        
+    </body>
+</html>
